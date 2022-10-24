498be667948625548923de0144cdd8e7f09519af
charity <wa.nelimacharity@gmail.com>
2022-10-24 12:43:37 +0300

    completed

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person 
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+
+
+end
+nelima = Person.new
+nelima.talk
+nelima.walk
